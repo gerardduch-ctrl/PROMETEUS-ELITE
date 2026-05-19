391e0744f7cebc6f8108bac526560bad5a489b6b
gerardduch-ctrl <gerardduch@gmail.com>
2026-05-19 14:39:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 st.markdown('<div class="main-title">PROMETEUS ELITE</div>', unsafe_allow_html=True)
-st.markdown('<div class="subtitle">PROMETEUS ELITE ULTRA</div>', unsafe_allow_html=True)
+st.markdown('<div class="subtitle">PROMETEUS ELITE ULTRA 6/50</div>', unsafe_allow_html=True)
 
 TRAMOS_DECENAS = {
     "1-10": list(range(1, 11)),
